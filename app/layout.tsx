b4b5caaf31c8f992c71ecf57cdb8bfc634c16a3d
Sergey <sayvix005@gmail.com>
2024-08-08 14:22:17 +0300

    editing next config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import NavLinks from "./ui/nav-links";
-export const experimental_ppr = true;
 import '@/app/ui/global.css';
 import { Metadata } from "next";
 import Script from "next/script";
@@ -12,10 +11,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="h-screen m-0">
-        <head>
+      <head>
           <Script src='https://telegram.org/js/telegram-web-app.js' strategy="beforeInteractive"/>
         </head>
+      <body className="h-screen m-0">
         <div className="flex h-full flex-col">
           <div className="flex-grow">{children}</div>
           <div className="p-2 fixed bottom-0 left-0 right-0 flex h-16 justify-between bg-white shadow-md gap-2">
